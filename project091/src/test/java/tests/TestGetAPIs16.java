04bf28d2dd8ce451b2da7d52f91c9e9464dd2309
kishore <skill2automate@gmail.com>
2025-12-04 11:18:50 +0530

    chanegs added 16

--- project091/src/test/java/tests/TestGetAPIs16.java
+++ project091/src/test/java/tests/TestGetAPIs16.java
@@ -31,8 +31,11 @@ public class TestGetAPIs16 {
 		Response res=req.put(data);	
 		
 		System.out.println(res.statusCode());
-//		System.out.println(res.asString());
-//		System.out.println(res.getHeaders());
+		System.out.println(res.asString());
+		System.out.println(res.getHeaders());
+		System.out.println(res.statusCode());
+		System.out.println(res.asString());
+		System.out.println(res.getHeaders());
 
 	
 	}
